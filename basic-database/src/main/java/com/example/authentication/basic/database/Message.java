c7905c9e5fe3519f4c0bc97c97dba2120adc0e6c
GaryChengld <chengld@yahoo.com>
2019-11-14 22:43:04 -0500

    basic authentication with database backed

---
+++ basic-database/src/main/java/com/example/authentication/basic/database/Message.java
@@ -0,0 +1,15 @@
+package com.example.authentication.basic.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Gary Cheng
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String message;
+}
