a46dfa5355a2206085cee3f6ae196da8b79cb180
GaryChengld <chengld@yahoo.com>
2019-12-08 20:59:04 -0500

    OAuth2 form login

--- oauth2-form/authorization-server/src/main/java/com/example/authentication/oauth/authserver/SecurityConfiguration.java
+++ oauth2-form/authorization-server/src/main/java/com/example/authentication/oauth/authserver/SecurityConfiguration.java
@@ -1,13 +1,14 @@
 package com.example.authentication.oauth.authserver;
 
 import com.example.authentication.oauth.authserver.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @author Gary Cheng
  */
 @Configuration
+@EnableWebSecurity
 @Order(1)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final CustomUserDetailsService customUserDetailsService;
@@ -24,10 +26,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .antMatchers("/h2/**", "/favicon.ico", "/error");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/h2/**").permitAll()
-                .and().requestMatchers()
+        http.requestMatchers()
                 .antMatchers("/login", "/oauth/authorize")
                 .and().authorizeRequests()
                 .anyRequest().authenticated()
