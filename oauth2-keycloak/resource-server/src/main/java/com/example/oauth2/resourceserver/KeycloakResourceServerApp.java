8536fb041cf87494e4e08758b63e63ced7a1f652
GaryChengld <chengld@yahoo.com>
2020-01-09 22:37:59 -0500

    OAuth2 keycloak

--- oauth2-keycloak/resource-server/src/main/java/com/example/oauth2/resourceserver/KeycloakResourceServerApp.java
+++ oauth2-keycloak/resource-server/src/main/java/com/example/oauth2/resourceserver/KeycloakResourceServerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeyCloakResourceServerApp {
+public class KeycloakResourceServerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KeyCloakResourceServerApp.class, args);
+		SpringApplication.run(KeycloakResourceServerApp.class, args);
 	}
 
 }
--- oauth2-keycloak/resource-server/src/main/java/com/example/oauth2/resourceserver/ResourceServerConfiguration.java
+++ oauth2-keycloak/resource-server/src/main/java/com/example/oauth2/resourceserver/ResourceServerConfiguration.java
@@ -1,17 +1,16 @@
 package com.example.oauth2.resourceserver;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 /**
  * @author Gary Cheng
@@ -20,18 +19,19 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 @EnableResourceServer
 @Order(1)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@ConditionalOnBean(KeycloakAccessTokenConverter.class)
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
     private static final String RESOURCE_ID = "account";
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/welcome").permitAll()
-                .antMatchers("/api/**").authenticated();
+                .antMatchers("/api/**").authenticated().and().oauth2ResourceServer();
     }
 
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId(RESOURCE_ID);
     }
-
 }
