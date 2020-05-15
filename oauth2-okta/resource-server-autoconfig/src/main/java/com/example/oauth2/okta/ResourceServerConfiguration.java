9a35848018f4164350bc584a47bf4a09ed6eaeb8
GaryChengld <chengld@yahoo.com>
2020-05-14 22:36:46 -0400

    oauth2 okta resource server auto config

--- oauth2-okta/resource-server-autoconfig/src/main/java/com/example/oauth2/okta/ResourceServerConfiguration.java
+++ oauth2-okta/resource-server-autoconfig/src/main/java/com/example/oauth2/okta/ResourceServerConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.oauth2.okta;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -15,4 +17,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @Order(1)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    private static final String RESOURCE_ID = "exampleResource";
+    @Value("${security.oauth2.resource.jwk.key-set-uri}")
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/welcome").permitAll()
+                .antMatchers("/api/**").authenticated();
+    }
+
 }
