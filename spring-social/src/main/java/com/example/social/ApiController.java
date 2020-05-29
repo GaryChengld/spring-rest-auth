2beabef97ebfea7a7c0300fb31c6c3031f57c0f8
GaryChengld <chengld@yahoo.com>
2020-05-28 22:43:54 -0400

    spring social

--- spring-social/src/main/java/com/example/social/ApiController.java
+++ spring-social/src/main/java/com/example/social/ApiController.java
@@ -2,6 +2,7 @@ package com.example.social;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ApiController {
+    @GetMapping("/public")
     public ApiResponse home() {
         return new ApiResponse("Public Api");
     }
 
+    @GetMapping("/user")
     public ApiResponse user() {
         return new ApiResponse("User Api");
     }
